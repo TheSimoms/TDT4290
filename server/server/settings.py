8f12db0cac1833a3f291047f1b0503618e8c4caf
Simon B-J <simon_bj@live.com>
2015-10-12 14:19:01 +0200

    Changed from JSON to XML in the API

--- server/server/settings.py
+++ server/server/settings.py
@@ -112,4 +112,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework_xml.parsers.XMLParser',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework_xml.renderers.XMLRenderer',
+    ),
 }
