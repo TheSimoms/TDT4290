7ba94894a88135a38db49f9589a89518d925a690
Simon B-J <simon_bj@live.com>
2015-10-14 11:45:58 +0200

    Added Python PIP to setup script

--- server/setup.sh
+++ server/setup.sh
@@ -4,4 +4,4 @@ export LC_ALL="en_US.UTF-8"
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install python-dev python-virtualenv sqlite3
+apt-get install python-dev python-virtualenv python-pip sqlite3
